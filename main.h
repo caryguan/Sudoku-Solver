2c862585bd1ea758135aeb0f01ca7f99f1a19159
z5160345 <36937392+z5160345@users.noreply.github.com>
2019-03-01 16:41:42 +1100

    fixed structs

--- main.h
+++ main.h
@@ -1,8 +1,11 @@
-void printHello();
+#include <stdlib.h>
 
-struct sudoku{
-    node[][] grid;
-}
-struct node{
-    int[] possibleValues;
-}
+typedef struct _node{
+    int possibleValues[9];
+}node;
+
+typedef struct _sudoku{
+    node grid[9][9];
+}sudoku;
+
+void printHello();
